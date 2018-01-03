c4e64ff99c4bdbac21c5a6ad241ce219639e78bd
rrcm16 <rrcm16@gmail.com>
2018-01-03 16:08:21 -0500

    Change name of navbar file

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,7 @@
 
 import { combineReducers } from 'redux';
 import sessionReducer from './session_reducer';
-import errorsReducer from './errors_reducer';
+import errorsReducer from './errors_Reducer';
 
 
 const rootReducer = combineReducers({
