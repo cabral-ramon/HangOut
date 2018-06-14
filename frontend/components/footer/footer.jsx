0476648294b9b67ce5ec6ccc629020352fc67943
Ramon Cabral <rrcm16@gmail.com>
2018-06-14 15:42:39 -0400

    Add link to profile to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 class Footer extends React.Component {
 
@@ -15,6 +16,12 @@ class Footer extends React.Component {
           <a target="_blank" href="https://www.linkedin.com/in/cabral-ramon/">
             <i className="fab fa-linkedin"></i>
           </a>
+        </div>
+
+        <div style={{color: 'white'}}>
+          <Typography color="inherit" variant="body1">
+            Website Created by <a style={{color: 'cadetblue'}}target="_blank" href="http://ramoncabral.com">Ramon Cabral</a>
+          </Typography>
         </div>
       </div>
     );
