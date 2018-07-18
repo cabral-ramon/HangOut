de18d2b8e9759cda5d1bae81f3edf5d473be7d7a
Ramon Cabral <rrcm16@gmail.com>
2018-07-17 22:45:47 -0400

    Refactor group menu onclick handlers

--- frontend/components/groups/group_show.jsx
+++ frontend/components/groups/group_show.jsx
@@ -17,15 +17,11 @@ class GroupShow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 0,
+      value: 0
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.leaveGroup = this.leaveGroup.bind(this);
     this.renderEventForm = this.renderEventForm.bind(this);
-    this.showEvents = this.showEvents.bind(this);
-    this.showComments = this.showComments.bind(this);
-    this.showAbout = this.showAbout.bind(this);
-    this.showMembers = this.showMembers.bind(this);
     this.showEditForm = this.showEditForm.bind(this);
     this.renderEditButton = this.renderEditButton.bind(this);
     this.renderDeleteButton = this.renderDeleteButton.bind(this);
@@ -49,81 +45,70 @@ class GroupShow extends React.Component {
     if (this.props.currentUser) {
       const groupId = this.props.group.id;
       const userId = this.props.currentUser.id;
-      const membership = Object.assign({}, { group_id: groupId, user_id: userId});
+      const membership = Object.assign(
+        {},
+        { group_id: groupId, user_id: userId }
+      );
       this.props.createMembership(membership);
     } else {
-      window.alert("Please log in to join a group.");
+      window.alert('Please log in to join a group.');
     }
   }
 
-  renderEventForm(){
-    if (this.props.currentUser && this.props.memberIds.includes(this.props.currentUser.id)) {
-      return (
-        <EventForm groupId={this.props.groupId}/>
-      );
+  renderEventForm() {
+    if (
+      this.props.currentUser &&
+      this.props.memberIds.includes(this.props.currentUser.id)
+    ) {
+      return <EventForm groupId={this.props.groupId} />;
     }
   }
 
   renderButton() {
     let buttonText;
-    if ( this.props.currentUser ) {
-      if ( this.props.memberIds.includes(this.props.currentUser.id) ) {
-        buttonText = "Leave Group";
+    if (this.props.currentUser) {
+      if (this.props.memberIds.includes(this.props.currentUser.id)) {
+        buttonText = 'Leave Group';
         return (
-          <button
-            className="group-show-button"
-            onClick={this.leaveGroup}>{buttonText}</button>
+          <button className="group-show-button" onClick={this.leaveGroup}>
+            {buttonText}
+          </button>
         );
       }
     }
-    buttonText = "Join Group";
+    buttonText = 'Join Group';
     return (
-      <button
-       className="group-show-button"
-       onClick={this.handleSubmit}
-       >{buttonText}</button>
+      <button className="group-show-button" onClick={this.handleSubmit}>
+        {buttonText}
+      </button>
     );
   }
 
-  showEvents(){
-    this.props.history.push(`/groups/${this.props.group.id}/events`);
+  showSection(section) {
+    this.props.history.push(
+      `/groups/${this.props.group.id}/${section ? section : ''}`
+    );
   }
 
-  showComments(){
-    this.props.history.push(`/groups/${this.props.group.id}/comments`);
-  }
-
-  showAbout(){
-    this.props.history.push(`/groups/${this.props.group.id}`);
-  }
-
-  showMembers(){
-    this.props.history.push(`/groups/${this.props.group.id}/members`);
-  }
-
-  showEditForm(){
+  showEditForm() {
     this.props.history.push(`/groups/${this.props.group.id}/edit`);
   }
 
-  renderEditButton(){
-    if(this.props.isOwner) {
-      return (
-        <Tab label="Edit" onClick={this.showEditForm}/>
-      );
+  renderEditButton() {
+    if (this.props.isOwner) {
+      return <Tab label="Edit" onClick={this.showEditForm} />;
     }
   }
 
   handleDelete(e) {
     e.preventDefault();
-    this.props.deleteGroup(this.props.groupId).then( (response)=> {
+    this.props.deleteGroup(this.props.groupId).then(response => {
       this.props.history.push('/homepage');
     });
   }
-  renderDeleteButton(){
-    if(this.props.isOwner) {
-      return (
-        <Tab label="Delete" onClick={this.handleDelete}/>
-      );
+  renderDeleteButton() {
+    if (this.props.isOwner) {
+      return <Tab label="Delete" onClick={this.handleDelete} />;
     }
   }
 
@@ -136,56 +121,76 @@ class GroupShow extends React.Component {
 
     if (this.props.group) {
       return (
-
         <div className="group-show-main">
-          <Link
-            to="/homepage"
-            className="back-to-link">back to Homepage</Link>
+          <Link to="/homepage" className="back-to-link">
+            back to Homepage
+          </Link>
           <div className="group-show-container">
             <section className="group-show-image-container">
-              <img className="group-show-image"
-                src={this.props.group.image}/>
+              <img className="group-show-image" src={this.props.group.image} />
               <div className="group-info-container">
-                <h2 className="group-show-name">
-                  {this.props.group.name}</h2>
+                <h2 className="group-show-name">{this.props.group.name}</h2>
                 <p>{this.props.group.location}</p>
                 {this.renderButton()}
               </div>
             </section>
             <nav className="group-show-menu-container">
               <Tabs value={value} onChange={this.handleChange}>
-                <Tab label="Group" onClick={this.showAbout}/>
-                <Tab label="Hangouts" onClick={this.showEvents}/>
-                <Tab label="Discussions" onClick={this.showComments}/>
-                <Tab label="Members" onClick={this.showMembers}/>
+                <Tab
+                  label="Group"
+                  onClick={this.showSection.bind(this, null)}
+                />
+                <Tab
+                  label="Hangouts"
+                  onClick={this.showSection.bind(this, 'events')}
+                />
+                <Tab
+                  label="Discussions"
+                  onClick={this.showSection.bind(this, 'comments')}
+                />
+                <Tab
+                  label="Members"
+                  onClick={this.showSection.bind(this, 'members')}
+                />
                 {this.renderEditButton()}
                 {this.renderDeleteButton()}
               </Tabs>
             </nav>
             <section className="group-show-section">
-              <Route exact path="/groups/:id" render={
-                  () => (
-                    <GroupAbout
-                      members={this.props.members}
-                      group={this.props.group}
-                      events={this.props.events}
-                      />
-                    )
-                  }
+              <Route
+                exact
+                path="/groups/:id"
+                render={() => (
+                  <GroupAbout
+                    members={this.props.members}
+                    group={this.props.group}
+                    events={this.props.events}
+                  />
+                )}
               />
-            <Route exact path="/groups/:id/events" render={
-                () => (
-                  <GroupEvents events={this.props.events}/>
-                )
-              }
+              <Route
+                exact
+                path="/groups/:id/events"
+                render={() => <GroupEvents events={this.props.events} />}
               />
-              <Route exact path="/groups/:id/comments" component={GroupCommentsContainer} />
-              <Route exact path="/groups/:id/members" component={GroupMembersContainer} />
-              <Route exact path="/groups/:id/edit" render={()=> <GroupEditContainer group={this.props.group}/>}/>
+              <Route
+                exact
+                path="/groups/:id/comments"
+                component={GroupCommentsContainer}
+              />
+              <Route
+                exact
+                path="/groups/:id/members"
+                component={GroupMembersContainer}
+              />
+              <Route
+                exact
+                path="/groups/:id/edit"
+                render={() => <GroupEditContainer group={this.props.group} />}
+              />
             </section>
-
+          </div>
         </div>
-      </div>
       );
     } else {
       return null;
