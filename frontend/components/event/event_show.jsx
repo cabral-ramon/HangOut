2ecd351904dbc3ce257a9b26df16ecf6bf7ee1de
Ramon Cabral <rrcm16@gmail.com>
2018-07-25 09:44:37 -0400

    Expand mobil image for event show page to 100%

--- frontend/components/event/event_show.jsx
+++ frontend/components/event/event_show.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
-import { dateToDayMonth, dateToWordDate } from '../../util/date_converter';
-import GroupMembers from '../groups/GroupMembers/group_members_container';
-
+import React from "react";
+import { withRouter, Link } from "react-router-dom";
+import { dateToDayMonth, dateToWordDate } from "../../util/date_converter";
+import GroupMembers from "../groups/GroupMembers/group_members_container";
 
 class EventShow extends React.Component {
   constructor(props) {
@@ -12,8 +11,6 @@ class EventShow extends React.Component {
     this.renderDayMonth = this.renderDayMonth.bind(this);
     this.renderWordDate = this.renderWordDate.bind(this);
   }
-
-
 
   componentDidMount() {
     this.props.fetchEvent(this.props.eventId);
@@ -41,42 +38,47 @@ class EventShow extends React.Component {
 
   renderButton() {
     let buttonText;
-    if ( this.props.currentUser ) {
-      if ( this.props.eventMembers.map( m => m.id ).includes( this.props.currentUser.id) ) {
+    if (this.props.currentUser) {
+      if (
+        this.props.eventMembers
+          .map(m => m.id)
+          .includes(this.props.currentUser.id)
+      ) {
         buttonText = "Cancel";
         return (
-          <button
-            className="event-rsvp-btn"
-            onClick={this.deleteRsvp}>{buttonText}</button>
+          <button className="event-rsvp-btn" onClick={this.deleteRsvp}>
+            {buttonText}
+          </button>
         );
       }
     }
     buttonText = "RSVP";
     return (
-      <button
-       className="event-rsvp-btn"
-       onClick={this.handleSubmit}
-       >{buttonText}</button>
+      <button className="event-rsvp-btn" onClick={this.handleSubmit}>
+        {buttonText}
+      </button>
     );
   }
 
-  renderDayMonth(){
-    if(this.props.event.date) {
+  renderDayMonth() {
+    if (this.props.event.date) {
       return (
         <div id="event-show-date">
-          <p id="event-show-day">{dateToDayMonth(this.props.event.date).slice(0,2)}</p>
-          <p id="event-show-month">{dateToDayMonth(this.props.event.date).slice(2,5)}</p>
+          <p id="event-show-day">
+            {dateToDayMonth(this.props.event.date).slice(0, 2)}
+          </p>
+          <p id="event-show-month">
+            {dateToDayMonth(this.props.event.date).slice(2, 5)}
+          </p>
         </div>
       );
     }
     return null;
   }
 
-  renderWordDate(){
-    if(this.props.event.date) {
-      return (
-        <span>{dateToWordDate(this.props.event.date)}</span>
-      );
+  renderWordDate() {
+    if (this.props.event.date) {
+      return <span>{dateToWordDate(this.props.event.date)}</span>;
     }
     return null;
   }
@@ -85,10 +87,11 @@ class EventShow extends React.Component {
     let rsvps;
     if (this.props.event) {
       if (this.props.eventMembers) {
-        rsvps = this.props.eventMembers.map( (member, idx) => {
+        rsvps = this.props.eventMembers.map((member, idx) => {
           return (
-            <li key={idx}
-              className="event-show-rsvp-list">{member.username}</li>
+            <li key={idx} className="event-show-rsvp-list">
+              {member.username}
+            </li>
           );
         });
       } else {
@@ -99,25 +102,28 @@ class EventShow extends React.Component {
       return (
         <div className="event-show-container">
           <div className="event-show-head-wrapper">
-            <Link
-              to="/homepage/events"
-              className="back-to-link">back to Events</Link>
+            <Link to="/homepage/events" className="back-to-link">
+              back to Events
+            </Link>
             <div className="event-show-head">
               <div className="page-head">
-                <div id="event-show-date-wrapper">
-                  {this.renderDayMonth()}
-                </div>
+                <div id="event-show-date-wrapper">{this.renderDayMonth()}</div>
                 <div className="event-show-name">
                   {this.renderWordDate()}
                   <h1>{this.props.event.name}</h1>
                   <p>Hosted by {this.props.organizer.username}</p>
-                  <p>From <Link className="general-link"
-                    to={`/groups/${this.props.group.id}`}>{this.props.group.name}</Link></p>
-                  <img style={{width: '50%', marginBottom: '20px'}} src={this.props.group.image} />
+                  <p>
+                    From{" "}
+                    <Link
+                      className="general-link"
+                      to={`/groups/${this.props.group.id}`}
+                    >
+                      {this.props.group.name}
+                    </Link>
+                  </p>
+                  <img src={this.props.group.image} />
                 </div>
-                <div className="event-rsvp-wrapper">
-                  {this.renderButton()}
-                </div>
+                <div className="event-rsvp-wrapper">{this.renderButton()}</div>
               </div>
             </div>
           </div>
@@ -130,7 +136,7 @@ class EventShow extends React.Component {
             <div className="event-show-time-location">
               <div>
                 <div id="event-show-time">
-                  <i className="far fa-clock"></i>
+                  <i className="far fa-clock" />
                   <p>{this.renderWordDate()}</p>
                 </div>
                 <div>
@@ -138,7 +144,7 @@ class EventShow extends React.Component {
                 </div>
               </div>
               <div id="event-show-location">
-                <i className="fas fa-map-marker-alt"></i>
+                <i className="fas fa-map-marker-alt" />
                 <p>{this.props.event.location}</p>
               </div>
             </div>
@@ -149,7 +155,6 @@ class EventShow extends React.Component {
       return null;
     }
   }
-
 }
 
 export default withRouter(EventShow);
