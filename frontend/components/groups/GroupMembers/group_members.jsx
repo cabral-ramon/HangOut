6b35e4f5f82e886e6d4612c032284fc17782b7a3
Ramon Cabral <rrcm16@gmail.com>
2018-08-31 08:20:50 -0400

    Add margin right to group member count

--- frontend/components/groups/GroupMembers/group_members.jsx
+++ frontend/components/groups/GroupMembers/group_members.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
+import React from "react";
+import { Link } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 
 class GroupMembers extends React.Component {
   constructor(props) {
@@ -13,33 +13,34 @@ class GroupMembers extends React.Component {
     if (this.props.members) {
       return (
         <div className="members-container">
-              <h3>
-                <span>({this.props.members.length})</span>
-                { this.props.title ? this.props.title : 'Members:'}
-              </h3>
-            <ul className="members-ul">
-              {this.props.members.map( (member) => (
-                <li key={member.id} className="member-wrapper">
-                  <Avatar alt={member.username} src={member.image}
-                    style={{
-                      width: 60,
-                      height: 60,
-                      alignSelf: 'center'
-                    }}
-                  />
-                  <Typography variant="subheading">
-                    {member.username}
-                  </Typography>
-                </li>
-              ))}
-            </ul>
+          <h3>
+            <span style={{ marginRight: "5px" }}>
+              ({this.props.members.length})
+            </span>
+            {this.props.title ? this.props.title : "Members:"}
+          </h3>
+          <ul className="members-ul">
+            {this.props.members.map(member => (
+              <li key={member.id} className="member-wrapper">
+                <Avatar
+                  alt={member.username}
+                  src={member.image}
+                  style={{
+                    width: 60,
+                    height: 60,
+                    alignSelf: "center"
+                  }}
+                />
+                <Typography variant="subheading">{member.username}</Typography>
+              </li>
+            ))}
+          </ul>
         </div>
       );
     } else {
       return null;
     }
   }
-
 }
 
 export default GroupMembers;
